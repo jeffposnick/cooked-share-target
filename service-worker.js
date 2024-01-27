121a359d4059148bbb647fe83395390fc5267f4c
Jeff Posnick <github@posnick.org>
2024-01-27 13:12:38 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,17 @@ self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
 
 self.addEventListener("fetch", (event) => {
+  console.log(event);
   const url = new URL(event.request.url);
+  console.log(url);
   if (
     event.request.method === "GET" &&
     url.pathname.endsWith("/share-target/")
   ) {
-    event.respondWith(
-      Response.redirect(`https://cooked.wiki/${url.searchParams.get("url")}`)
+    const redirectUrl = new URL(
+      `/${url.searchParams.get("recipeUrl")}`,
+      "https://cooked.wiki"
     );
+    event.respondWith(Response.redirect(redirectUrl.href));
   }
 });
